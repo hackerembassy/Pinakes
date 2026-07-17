c697a1a18ca3dac84783dd89fdc62616ab1ecb47
Vladislav Glagolev <glagol15@gmail.com>
2026-07-17 19:31:54 +0400

    Refactor custom CSS handling in partials

--- app/Views/auth/partials/custom-css.php
+++ app/Views/auth/partials/custom-css.php
@@ -4,9 +4,8 @@ use App\Support\ConfigStore;
 use App\Support\ContentSanitizer;
 
 $customCss = ConfigStore::get('advanced.custom_header_css', '');
-$customCss = is_string($customCss) ? ContentSanitizer::normalizeExternalAssets($customCss) : '';
-
-if ($customCss !== ''):
+$customCss = is_string($customCss) ? ContentSanitizer::normalizeExternalAssets($customCss) : $customCss;
+if (!empty($customCss)):
     ?>
     <style>
         <?= $customCss ?>
